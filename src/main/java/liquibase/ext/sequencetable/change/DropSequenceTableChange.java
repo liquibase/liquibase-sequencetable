9bce14eb0ffd96d48e5781151db8395a55f42bf6
Xavier Poinsard <xpoinsard@openpricer.com>
2014-07-22 11:36:09 +0200

    fix missing annotation for DropSequenceTableChange

--- src/main/java/liquibase/ext/sequencetable/change/DropSequenceTableChange.java
+++ src/main/java/liquibase/ext/sequencetable/change/DropSequenceTableChange.java
@@ -1,71 +1,71 @@
 package liquibase.ext.sequencetable.change;
 
 import liquibase.change.AbstractChange;
+import liquibase.change.ChangeMetaData;
+import liquibase.change.DatabaseChange;
 import liquibase.database.Database;
-import liquibase.datatype.core.BigIntType;
 import liquibase.statement.SqlStatement;
-import liquibase.statement.core.CreateTableStatement;
 import liquibase.statement.core.DropTableStatement;
-import liquibase.statement.core.InsertStatement;
 
-import java.math.BigInteger;
-
+@DatabaseChange(name="dropSequenceTable",
+description = "Drop the table used to generate sequence values if the database does not support sequences",
+priority = ChangeMetaData.PRIORITY_DEFAULT)
 public class DropSequenceTableChange extends AbstractChange {
 
-    private String catalogName;
-    private String schemaName;
-    private String tableName;
-    private boolean alwaysUse = false;
+	private String catalogName;
+	private String schemaName;
+	private String tableName;
+	private boolean alwaysUse = false;
 
-    public String getCatalogName() {
-        return catalogName;
-    }
+	public String getCatalogName() {
+		return catalogName;
+	}
 
-    public void setCatalogName(String catalogName) {
-        this.catalogName = catalogName;
-    }
+	public void setCatalogName(String catalogName) {
+		this.catalogName = catalogName;
+	}
 
-    public String getSchemaName() {
-        return schemaName;
-    }
+	public String getSchemaName() {
+		return schemaName;
+	}
 
-    public void setSchemaName(String schemaName) {
-        this.schemaName = schemaName;
-    }
+	public void setSchemaName(String schemaName) {
+		this.schemaName = schemaName;
+	}
 
-    public String getTableName() {
-        return tableName;
-    }
+	public String getTableName() {
+		return tableName;
+	}
 
-    public void setTableName(String tableName) {
-        this.tableName = tableName;
-    }
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
 
-    public boolean isAlwaysUse() {
-        return alwaysUse;
-    }
+	public boolean isAlwaysUse() {
+		return alwaysUse;
+	}
 
-    public void setAlwaysUse(boolean alwaysUse) {
-        this.alwaysUse = alwaysUse;
-    }
+	public void setAlwaysUse(boolean alwaysUse) {
+		this.alwaysUse = alwaysUse;
+	}
 
-    @Override
-    public String getConfirmationMessage() {
-        return "Sequence table "+tableName+" dropped";
-    }
+	@Override
+	public String getConfirmationMessage() {
+		return "Sequence table "+tableName+" dropped";
+	}
 
-    @Override
-    public SqlStatement[] generateStatements(Database database) {
+	@Override
+	public SqlStatement[] generateStatements(Database database) {
 
 
-        if (alwaysUse || !database.supportsSequences()) {
-            DropTableStatement tblStmt = new DropTableStatement(this.catalogName, this.schemaName, this.tableName, false);
+		if (alwaysUse || !database.supportsSequences()) {
+			DropTableStatement tblStmt = new DropTableStatement(this.catalogName, this.schemaName, this.tableName, false);
 
-            return new SqlStatement[] {
-                    tblStmt,
-            };
-        } else {
-            return new SqlStatement[0];
-        }
-    }
+			return new SqlStatement[] {
+					tblStmt,
+			};
+		} else {
+			return new SqlStatement[0];
+		}
+	}
 }
